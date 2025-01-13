631df419e55726c54028e3486b39d95ac1dc7bb5
Viktor Shterev <viktor.shterev55@gmail.com>
2025-01-13 12:36:07 +0200

    added unit tests on brand rest service

--- src/test/java/bg/softuni/campingcars/service/impl/BrandServiceImplTest.java
+++ src/test/java/bg/softuni/campingcars/service/impl/BrandServiceImplTest.java
@@ -1,125 +1,201 @@
-//package bg.softuni.campingcars.service.impl;
-//
-//import bg.softuni.campingcars.model.dto.bindingModels.BrandDTO;
-//import bg.softuni.campingcars.model.dto.bindingModels.BrandModelAddBindingModel;
-//import bg.softuni.campingcars.model.enums.CategoryEnum;
-//import bg.softuni.campingcars.repository.CategoryRepository;
-//import bg.softuni.campingcars.repository.ModelRepository;
-//import bg.softuni.campingcars.service.BrandService;
-//import bg.softuni.campingcars.testUtils.TestDataUtil;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//@SpringBootTest
-//class BrandServiceImplTest {
-//
-//    @Autowired
-//    private BrandRepository brandRepository;
-//
-//    @Autowired
-//    private TestDataUtil testDataUtil;
-//
-//    @Autowired
-//    private BrandService brandService;
-//
-//    @Autowired
-//    private ModelRepository modelRepository;
-//
-//    @Autowired
-//    private CategoryRepository categoryRepository;
-//
-//    @BeforeEach
-//    void setUp() {
-//        this.testDataUtil.cleanUp();
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        this.testDataUtil.cleanUp();
-//    }
-//
-//    @Test
-//    void testBrandRepositoryReturnEmptyList() {
-//
-//        List<BrandDTO> allBrands = this.brandService.getAllBrands();
-//
-//        assertEquals(0, allBrands.size());
-//    }
-//
-//    @Test
-//    void testBrandRepositoryReturns() {
-//
-//        this.testDataUtil.creatingTestBrand();
-//
-//        List<BrandDTO> allBrands = this.brandService.getAllBrands();
-//
-//        assertEquals(1, allBrands.size());
-//        assertEquals("Test Brand", allBrands.get(0).name());
-//        assertEquals(1, allBrands.get(0).models().size());
-//        assertEquals("Test Model", allBrands.get(0).models().stream().findFirst().get().name());
-//        assertEquals("CAMPER", allBrands.get(0).models().stream().findFirst().get().category());
-//    }
-//
-//    @Test
-//    void testBrandAdd() {
-//
-//        BrandModelAddBindingModel brandModelAddBindingModel = new BrandModelAddBindingModel("New Brand", "New Model", CategoryEnum.CARAVAN);
-//
-//        this.brandService.addBrandModel(brandModelAddBindingModel);
-//
-//        assertTrue(this.brandRepository.findByName("New Brand").isPresent());
-//        assertTrue(this.modelRepository.findByName("New Model").isPresent());
-//    }
-//
-//    @Test
-//    void testBrandUpdate() {
-//        Brand brand = this.testDataUtil.creatingTestBrand();
-//
-//        BrandModelAddBindingModel brandModelAddBindingModel = new BrandModelAddBindingModel("Test Brand", "New Model", CategoryEnum.CARAVAN);
-//
-//        assertTrue(this.brandRepository.findByName(brand.getName()).isPresent());
-//        assertTrue(this.modelRepository.findByName(brand.getModels().iterator().next().getName()).isPresent());
-//
-//        assertEquals("Test Brand", brand.getName());
-//        assertEquals("Test Model", brand.getModels().iterator().next().getName());
-//        assertEquals("CAMPER", brand.getModels().iterator().next().getCategory().getCategory().name());
-//
-//        this.brandService.addBrandModel(brandModelAddBindingModel);
-//
-//        Brand updatedBrand = this.brandRepository.findByName(brand.getName()).get();
-//
-//        assertEquals(2, updatedBrand.getModels().size());
-//        assertEquals("New Model", updatedBrand.getModels().stream().skip(1).findFirst().get().getName());
-//        assertEquals("CARAVAN", updatedBrand.getModels().stream().skip(1).findFirst().get().getCategory().getCategory().name());
-//
-//        assertTrue(this.modelRepository.findByName(updatedBrand.getModels().stream().skip(1).findFirst().get().getName()).isPresent());
-//    }
-//
-//    @Test
-//    void testBrandUpdateThrowsInvalidCategory() {
-//        Brand brand = this.testDataUtil.creatingTestBrand();
-//
-//        BrandModelAddBindingModel brandModelAddBindingModel = new BrandModelAddBindingModel("Test Brand", "New Model", null);
-//
-//        assertTrue(this.brandRepository.findByName(brand.getName()).isPresent());
-//        assertTrue(this.modelRepository.findByName(brand.getModels().iterator().next().getName()).isPresent());
-//
-//        assertEquals("Test Brand", brand.getName());
-//        assertEquals("Test Model", brand.getModels().iterator().next().getName());
-//        assertEquals("CAMPER", brand.getModels().iterator().next().getCategory().getCategory().name());
-//
-//        RuntimeException exception = assertThrows(
-//                RuntimeException.class,
-//                () -> brandService.addBrandModel(brandModelAddBindingModel)
-//        );
-//
-//        assertEquals("Category not found", exception.getMessage());
-//    }
-//}
+package bg.softuni.campingcars.service.impl;
+
+import bg.softuni.campingcars.model.dto.bindingModels.BrandDTO;
+import bg.softuni.campingcars.model.dto.bindingModels.BrandModelAddBindingModel;
+import bg.softuni.campingcars.model.dto.bindingModels.brandRestDtos.BrandRestDTO;
+import bg.softuni.campingcars.model.dto.bindingModels.brandRestDtos.ModelRestDTO;
+import bg.softuni.campingcars.model.entity.Category;
+import bg.softuni.campingcars.model.entity.Model;
+import bg.softuni.campingcars.model.enums.CategoryEnum;
+import bg.softuni.campingcars.repository.CategoryRepository;
+import bg.softuni.campingcars.repository.ModelRepository;
+import bg.softuni.campingcars.repository.OfferRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class BrandServiceImplTest {
+
+    private BrandServiceImpl toTest;
+
+    @Mock
+    private ModelRepository mockModelRepository;
+
+    @Mock
+    private OfferRepository mockOfferRepository;
+
+    @Mock
+    private CategoryRepository mockCategoryRepository;
+
+    @Mock
+    private RestClient mockRestClient;
+
+    @Mock
+    private RestClient.RequestHeadersUriSpec requestHeadersUriSpec;
+
+    @Mock
+    private RestClient.RequestHeadersSpec requestHeadersSpec;
+
+    @Mock
+    private RestClient.ResponseSpec responseSpec;
+
+    @Mock
+    private RestClient.RequestBodyUriSpec requestBodyUriSpec;
+
+    @Mock
+    private RestClient.RequestBodySpec requestBodySpec;
+
+    @Mock
+    private UserDetails principal;
+
+    @BeforeEach
+    void setUp() {
+        toTest = new BrandServiceImpl(mockModelRepository, mockCategoryRepository, mockOfferRepository, mockRestClient);
+    }
+
+    @Test
+    void testGetAllBrandsReturnsValue() {
+        BrandRestDTO brandRestDTO = createBrandRestDTO();
+
+        when(mockRestClient.get()).thenReturn(requestHeadersUriSpec);
+        when(requestHeadersUriSpec.uri("/brands/all")).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.accept(MediaType.APPLICATION_JSON)).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+        when(responseSpec.body(new ParameterizedTypeReference<List<BrandRestDTO>>() {
+        })).thenReturn(List.of(brandRestDTO));
+
+//        when(mockRestClient.get()
+//                .uri("/brands/all")
+//                .accept(MediaType.APPLICATION_JSON)
+//                .retrieve()
+//                .body(new ParameterizedTypeReference<>() {
+//                }))
+//                .thenReturn(brandRestDTO);
+
+        when(mockModelRepository.findAllByBrandName(brandRestDTO.name()))
+                .thenReturn(List.of(new Model().setBrandName("test").setName("Cool").setCategory(new Category().setCategory(CategoryEnum.CAMPER))));
+
+        List<BrandDTO> allBrands = toTest.getAllBrands();
+        assertNotNull(allBrands);
+        assertFalse(allBrands.isEmpty());
+        assertEquals(1, allBrands.size());
+    }
+
+    @Test
+    void testGetAllBrandsReturnsEmpty() {
+
+        when(mockRestClient.get()).thenReturn(requestHeadersUriSpec);
+        when(requestHeadersUriSpec.uri("/brands/all")).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.accept(MediaType.APPLICATION_JSON)).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
+        when(responseSpec.body(new ParameterizedTypeReference<List<BrandRestDTO>>() {
+        })).thenReturn(List.of());
+
+//        when(mockRestClient.get()
+//                .uri("/brands/all")
+//                .accept(MediaType.APPLICATION_JSON)
+//                .retrieve()
+//                .body(new ParameterizedTypeReference<>() {
+//                }))
+//                .thenReturn(brandRestDTO);
+
+        List<BrandDTO> allBrands = toTest.getAllBrands();
+        assertNotNull(allBrands);
+        assertTrue(allBrands.isEmpty());
+    }
+
+    @Test
+    void testAddBrandModelWorks() {
+        BrandModelAddBindingModel bindingModel = new BrandModelAddBindingModel("BrandX", "ModelY", CategoryEnum.CAMPER);
+
+        Category category = new Category().setCategory(CategoryEnum.CAMPER);
+
+        Model savedModel = (Model) new Model().setBrandName("test").setName("Cool").setCategory(new Category().setCategory(CategoryEnum.CAMPER)).setId(1L);
+
+        when(mockCategoryRepository.findByCategory(CategoryEnum.CAMPER)).thenReturn(Optional.of(category));
+        when(mockModelRepository.save(any(Model.class))).thenReturn(savedModel);
+
+        when(mockRestClient.post()).thenReturn(requestBodyUriSpec);
+        when(requestBodyUriSpec.uri("/brands/add")).thenReturn(requestBodySpec);
+        when(requestBodySpec.body(any(BrandRestDTO.class))).thenReturn(requestBodySpec);
+        when(requestBodySpec.retrieve()).thenReturn(null); // Simulate successful request
+
+        toTest.addBrandModel(bindingModel);
+
+        verify(mockCategoryRepository, times(1)).findByCategory(CategoryEnum.CAMPER);
+        verify(mockModelRepository, times(1)).save(any(Model.class));
+        verify(mockRestClient, times(1)).post();
+        verify(requestBodyUriSpec, times(1)).uri("/brands/add");
+        verify(requestBodySpec, times(1)).body(any(BrandRestDTO.class));
+    }
+
+
+    @Test
+    void testDeleteOfferShouldDeleteWhenAdmin() {
+
+        String brandName = "test";
+        UserDetails principal = new User("username", "password", List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+
+        Model model = new Model();
+        model.setId(1L);
+        when(mockModelRepository.findAllByBrandName(brandName)).thenReturn(List.of(model));
+
+        when(mockOfferRepository.findAllByModelIdIn(any())).thenReturn(List.of());
+
+        when(mockRestClient.delete()).thenReturn(requestHeadersUriSpec);
+        when(requestHeadersUriSpec.uri(any(Function.class))).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(mock(RestClient.ResponseSpec.class));
+
+        // Act
+        toTest.deleteOffer(brandName, principal);
+
+        // Assert
+        verify(mockModelRepository, times(1)).findAllByBrandName(brandName);
+        verify(mockOfferRepository, times(1)).findAllByModelIdIn(any());
+        verify(mockModelRepository, times(1)).deleteAllByBrandName(brandName);
+
+        // Verify RestClient DELETE interaction
+        verify(mockRestClient, times(1)).delete();
+        verify(requestHeadersUriSpec, times(1)).uri(any(Function.class));
+        verify(requestHeadersSpec, times(1)).retrieve();
+    }
+
+    @Test
+    void deleteOffer_ShouldNotDeleteOrCallRestClient_WhenNotAdmin() {
+
+        String brandName = "test";
+
+        UserDetails principal = new User("username", "password", List.of(new SimpleGrantedAuthority("ROLE_USER")));
+
+        toTest.deleteOffer(brandName, principal);
+
+        verify(mockModelRepository, never()).findAllByBrandName(brandName);
+        verify(mockOfferRepository, never()).findAllByModelIdIn(any());
+        verify(mockModelRepository, never()).deleteAllByBrandName(any());
+        verify(mockRestClient, never()).delete();
+    }
+
+    private BrandRestDTO createBrandRestDTO() {
+        return new BrandRestDTO("test", Set.of(new ModelRestDTO(1L)));
+    }
+}
